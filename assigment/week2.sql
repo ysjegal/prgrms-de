d423f1489a6e5cf8910ed1de3944d77858227922
ysjegal <67741779+ysjegal@users.noreply.github.com>
2020-07-29 15:29:13 +0900

    Update week2.sql

--- assigment/week2.sql
+++ assigment/week2.sql
@@ -1,32 +1,17 @@
 -- 2주차 과제
 
+/* 코딩 숙제: SQL이나  Python으로 Monthly Active User 세보기
+ - 앞서 주어진 두개의 테이블 (raw_data.session_timestamp, raw_data.user_session_channel)을 바탕으로 월별마다 액티브한 사용자들의 수를 카운트한다.
+ - 여기서 중요한 점은 세션의 수를 세는 것이 아니라 사용자의 수를 카운트한다는 점이다.
+ - 결과는 예를 들면 아래와 같은 식이 되어야 한다:
+   - 2019-05: 400
+   - 2019-06: 500
+   - 2019-07: 600
+   - ... */
+
 SELECT TO_CHAR(ts, 'yyyy-mm') m, COUNT(DISTINCT userid)
 FROM raw_data.session_timestamp
 INNER JOIN raw_data.user_session_channel
 USING(sessionid)
 GROUP BY m
 ORDER BY m;
-
-
-/* <쇼핑몰 데이터 웨어하우스>
-
-raw_data에 넣을 데이터
-
-상품 구매 데이터  (product) 
-- 구매된 상품명 등 방문자의 상품구매 정보
-검색어 
-- 상품을 구매한 방문자가 검색창에 입력한 검색어에 대한 검색량
-- 기간 내 일별/검색어별 검색량
-회원 
-- 방문자의 성별/연령 정보
-세션 
-- 상품을 구매한 방문자의 세션 접속일, 총 세션시간, 세션 기준 발생지역 등
-상품 
-- 상품명 등
-
-analytics 
-- 성별/나이 정보
-- 매출 정보
-- 구매 상품 정보
-... */
-
